673c5e89cd6dfda0bfde63b21f61cd920058e50b
Duc Pham Minh <terangmd113@gmail.com>
2023-06-28 13:58:52 +0700

    23.06.28, Update deploy_open_pdks.sh

--- deploy_open_pdks.sh
+++ deploy_open_pdks.sh
@@ -15,7 +15,7 @@ cd ~
 ### Get the required files for open_pdks
 git clone https://github.com/RTimothyEdwards/open_pdks && cd open_pdks
 ### Configure Skywater SKY130nm PDK installation
-./configure 	--enable-sky130-pdk=${skywater_dir}	\
+./configure 	--enable-sky130-pdk=${skywater_dir}	\	
 		--enable-sram-sky130			\
 		--enable-alpha-sky130
 ### The following steps will take a while to complete, be patience
@@ -24,6 +24,25 @@ make -j$(nproc)
 ### If make process encountered an error, use this command to clean the directory & start over
 # make distclean
 sudo make install
+# 
+cd ~
+
+### 
 # ***
 # ------------------------------------------------------------------------------------
 
+# Proceed to make the test by viewing sky130A GDS file in Magic VLSI tools
+# ***
+# If Magic VLSI's console return the error below
+# Error: Don't know how to read GDS-II. Nothing in "cifinput" section of tech file.
+# 
+# This error is caused by either:
+## Magic VLSI invoke command is incorrect. The correct command for invoking & test a specific PDK is:
+# magic -T sky130A; # With sky130A is the name of the PDK you want to test
+#
+## Or the .tech file of the PDK was not inserted to Magic VLSI's sys start-up directory. Execute these command to fix:
+# sudo cp ~/open_pdks/sky130/sky130A/libs.tech/magic/sky130A*.tech /opt/magic-8.3/lib/magic/sys/
+# sudo cp ~/open_pdks/sky130/sky130B/libs.tech/magic/sky130B*.tech /opt/magic-8.3/lib/magic/sys/
+## Then try the invoke command again. You should be good to go
+
+# ------------------------------------------------------------------------------------
